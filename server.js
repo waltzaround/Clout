01abe478d311f49fb5ace1abba0297177da074f1
minh <Lminhphu0304@gmail.com>
2021-02-20 15:01:09 +1300

    Add basic middleware for express 0auth

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express')
 const bodyParser = require('body-parser');
-const next = require('next')
+const next = require('next');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 
 const { backendRequestHandler } = require('./api');
+
+const passportStrategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+    process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/api/user/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
 
 const port = parseInt(process.env.PORT, 10) || 8000
 const dev = process.env.NODE_ENV !== 'production'
@@ -13,7 +34,10 @@ app.prepare().then(() => {
   const server = express()
   server.use(bodyParser.urlencoded({ extended: true }))
   server.use(bodyParser.json())
+  passport.use(passportStrategy);
 
+  server.use(passport.initialize());
+  server.use(passport.session());
   server.use('/api', backendRequestHandler);
   server.all('*', (req, res) => {
     return nextJsRequestHandler(req, res)
