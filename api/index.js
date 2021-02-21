cffcb846c4d12cff46f800d441cf1794c2e7e93f
minh <Lminhphu0304@gmail.com>
2021-02-21 15:50:29 +1300

    Add activity router to Express app route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const { internalRoute } = require('./internal');
 const { userRoute } = require('./users');
+const { activityRoutes } = require('./activities');
 
 const backendRequestHandler = Router();
 backendRequestHandler.use('/internal', internalRoute);
 backendRequestHandler.use('/user', userRoute);
+backendRequestHandler.use('/activity', activityRoutes);
 
 module.exports = {
   backendRequestHandler
